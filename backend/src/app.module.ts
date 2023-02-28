9fb11486ad2e83ec2cc838d4e222d553e4e43f28
Kevin Troude <ktroude@c1r4p6.42nice.fr>
2023-02-28 21:14:58 +0100

    starting encoding API string

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    AuthModule, 
+    UserModule, 
+    BookmarkModule, 
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,13 +1,57 @@
-import { Injectable } from "@nestjs/common";
-import { User, Bookmark } from "@prisma/client"
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2"
+import { PrismaClientInitializationError, PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-    signup() {
-        return {msg: 'signed up'}
+    constructor(private prisma: PrismaService) {}
+
+    async signup(dto: AuthDto) {
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        try {
+            // save new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }})
+
+            delete user.hash // delete hashed PW from the return for safety
+
+            // return the saved user
+            return user;
+        }
+        catch(error) {
+            if (error) {
+                if (error.code === 'P2002') // num de ce code --> cette variable unique existe deja
+                    throw new ForbiddenException('This email is already taken');
+                throw error;
+            }
+        }
     }
 
-    signin() {
-        return {msg: 'signed in'}
+    async signin(dto: AuthDto) {
+        // find the user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        });
+        console.log(user);
+        // if user does not exist throw exeption
+        if (!user)
+            throw new ForbiddenException('Incorect email');
+        // compare PW
+        const pwMatches = await argon.verify(user.hash, dto.password);
+        // if PW incorect throw exeption
+        if (!pwMatches)
+            throw new ForbiddenException('Incorect password')
+        // send back the user
+        delete user.hash
+        return user;
     }
 }
