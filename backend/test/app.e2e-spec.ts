db7ec25690a9ee79bc0c567d6cb6c45e81d1943a
Benoit Leroy <bleroy@c1r8p1.42nice.fr>
2023-03-04 12:24:19 +0100

    Syntax fix on (User    Get Me  ✓ should return current user )

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing'
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
-import * as pactum from 'pactum'
+import * as pactum from 'pactum';
 import { AuthDto } from 'src/auth/dto';
 
 describe('App test', () => {
@@ -13,10 +13,10 @@ describe('App test', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes( new ValidationPipe({ whitelist: true }));
     await app.init();
-    await app.listen(3333);
+    await app.listen(3334);
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
-    pactum.request.setBaseUrl('http://localhost:3333')
+    pactum.request.setBaseUrl('http://localhost:3334')
   });
   
   afterAll(() => { app.close() });
@@ -54,7 +54,7 @@ describe('App test', () => {
   describe('User', () => {
     describe('Get me', () => {
       it('should return current user', () => {
-        return pactum.spec().get('/users/me').withHeaders({ Authorization: 'Bearer $S(userAt)'}).expectStatus(200).inspect();
+        return pactum.spec().get('/users/me').withHeaders({ Authorization: 'Bearer $S{userAt}'}).expectStatus(200).inspect();
       })
     });
     describe('Edit user', () => {});
