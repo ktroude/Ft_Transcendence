0c53242f2d35684cccf61a45d9190dd783f70fef
Kevin Troude <ktroude@c1r4p6.42nice.fr>
2023-03-12 00:18:16 +0100

    hidding sus variable

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-import * as FormData from 'form-data';
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+// permet de charger des variables depuis le fichier .env a la racine de backend
+// c'est une question de secu pour ne pas laisser le client secret et autre variable sensible dans le code
+dotenv.config();
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma:PrismaService) {}
-   
+    
     // cette fonction permet de recuperer toutes les info d'un pelo qui se connecte a notre site via ce lien:
     // https://api.intra.42.fr/oauth/authorize
     async getUserData(code: string): Promise<any> {
         // url pour avoir le token genere par l'api 42
-        const token_url = 'https://api.intra.42.fr/oauth/token';
+        const token_url = process.env.TOKEN_URL;
         // les infos a envoyer a cette api
         const data = {
-          grant_type: 'authorization_code',
-          client_id: 'u-s4t2ud-ad63634a67eaff34218824a7796fa6e5c5337b11f88c026df3956ba7a8f38dc6',
-          client_secret: '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',
+          grant_type: process.env.GRANT_TYPE ,
+          client_id: process.env.CLIENT_ID ,
+          client_secret: process.env.CLIENT_SECRET,
           code: code,
-          redirect_uri: 'http://localhost:3000/getUserData',
+          redirect_uri: process.env.REDIRECT_URI ,
         };
         // la requete a l'api de 42
         const response = await axios.post(token_url, data);
         // url pour echanger le token (qu'on vient de generer dans la variable response) contre les data du user
-        const data_url = 'https://api.intra.42.fr/v2/me';
+        const data_url = process.env.DATA_URL;
         // le token en question
         const access_token = response.data.access_token;
         const headers = {
@@ -31,7 +35,7 @@ export class AuthService {
         };
         // la requete pour faire l'echange
         const userResponse = await axios.get(data_url, { headers: headers });
-        // console.log(userResponse.data);
+        // toute les infos sont donc contenue dans userResponse.data 
         return userResponse.data;
       }
     
