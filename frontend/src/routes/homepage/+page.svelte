613b04b1c2ed4cd82d1256f0565e1934e04b6075
Paco Le berre <pacocmoi@live.fr>
2023-04-17 17:21:57 +0200

    cleaned homepage

--- frontend/src/routes/homepage/+page.svelte
+++ frontend/src/routes/homepage/+page.svelte
@@ -55,13 +55,9 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { goto } from "$app/navigation";
-  import {fetchData, fetchFriend, fetchAccessToken} from "../../API/api";
+  import {fetchData, fetchFriend} from "../../API/api";
 
-  let previousFriend: string;
-  let showButtons = false;
-  let clickedFriend: string;
   let friends = [];
-  let friendNameAdd: string;
 
   let user: User;
     interface User {
@@ -69,80 +65,6 @@
         pseudo: string;
         firstName: string;
         lastName: string;
-    }
-
-  function handleFriendClick(friendName: string) {
-    clickedFriend = friendName;
-    setShowButtons(previousFriend !== clickedFriend ? true : !showButtons);
-    previousFriend = clickedFriend;
-  }
-
-  async function setShowButtons(value: boolean) {
-    showButtons = value;
-    console.log("showButtons: " + showButtons);
-  }
-
-  async function handleMessageFriend(friendName) {
-    console.log(`Sending message to ${friendName}`);
-  }
-
-  async function handleProfileFriend(friendName) {
-  const accessToken = await fetchAccessToken();
-    if (accessToken) 
-    {
-      navigate(`/${friendName}`);
-    } 
-    else 
-      console.log('Error: Could not get profile');
-  }
-
-  async function handleDeleteFriend(friendName) {
-    const accessToken = await fetchAccessToken();
-    if (accessToken)
-    {
-      const response = await fetch(`http://localhost:3000/users/${user.pseudo}/deletefriend`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
-            },
-            body: JSON.stringify({friend: friendName })
-        });
-        if (response.ok)
-            friends = friends.filter(friend => friend !== friendName);
-        else
-            console.log('Error: Could not delete friend');
-      }
-      else
-        console.log('Error: Could not delete friend');
-    }
-
-  function handleInviteFriend(friendName) {
-    console.log(`Inviting ${friendName} to play`);
-  }
-
-    const  handleAddFriend = async () => {
-        await fetchData();
-        if (!friendNameAdd)
-            return;
-        const accessToken = await fetchAccessToken();
-        if (accessToken) {
-            const response = await fetch(`http://localhost:3000/users/${user.pseudo}/friend`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
-            },
-            body: JSON.stringify({friend: friendNameAdd })
-        });
-        if (response.ok)
-        {
-            friends = await fetchFriend(user.pseudo);
-            friendNameAdd = '';
-        }
-        else
-            console.log('Error: Could not add friend');
-        }
     }
 
     const loadpage = async () =>
@@ -153,10 +75,6 @@
             friends = listFriends;
         else
             console.log('Error: Could not get friends');
-    }
-
-    const gotoRoute = (route: string) => {
-        goto("http://localhost:5173" + route);
     }
 
     onMount(() => {
