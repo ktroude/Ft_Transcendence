aaa12111162e2de281b10fd365e024d767c2ce5f
Paco Le berre <pacocmoi@live.fr>
2023-04-27 13:47:40 +0200

    fixed pb when lauching back

--- backend/src/chat/chatRoom.module.ts
+++ backend/src/chat/chatRoom.module.ts
@@ -5,11 +5,12 @@ import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
 import { ChatRoomController } from './chatRoom.controller';
 import { ChatRoomGateway } from './chatRoom.gateway';
+import { BlockService } from 'src/block/block.service';
 import { ChatRoomService } from './chatRoom.service';
 
 @Module({
   imports: [PrismaModule, JwtModule],
   controllers: [ChatRoomController],
-  providers: [ChatRoomService, ChatRoomGateway, UserService],
+  providers: [ChatRoomService, ChatRoomGateway, UserService, BlockService],
 })
 export class ChatRoomModule {}
