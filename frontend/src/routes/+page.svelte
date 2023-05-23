47eff6afc62ee336729ea1010a4a64f47232c792
Benoit Leroy <bleroy@c2r7p9.42nice.fr>
2023-05-23 14:22:19 +0200

    Refresh API token

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 
 <h1>Ft Transcendence</h1>
 <h2>
-    clique <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-4152a6ac095bc40a8ad0143aed98b074723631fc2398a0c3b359f278c7e1ae2c&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ftoken&response_type=code">
+    clique <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-50f3e66e6fe031e7c81489010de827d9db8e756fa49de5d8760e1722d283c275&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ftoken&response_type=code">
         ici 
     </a> pour te connecter 
 </h2>
+
+<!-- https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-fbca848ccb6f0757b4c503e49b4c9541f26f62ac877036ed0f6c44afd5e91dfe&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Ftoken&response_type=code -->
