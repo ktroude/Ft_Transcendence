523e40c825b7c517b501fd28b8d1fde7868dabe9
Paco Le berre <pacocmoi@live.fr>
2023-05-27 17:01:44 +0200

    Game is responsive, added animation to scores and few things idk

--- frontend/src/routes/game/pong_game/+page.svelte
+++ frontend/src/routes/game/pong_game/+page.svelte
@@ -10,29 +10,32 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 </svelte:head>
 
-
 <!-- {#if loading == true} -->
 <body style="margin:0px; padding:0px; background-image:url('/img/bg2.jpeg');
-background-position: center; background-size: cover ; overflow: hidden; width: 100vw;height: 100vh">
-		
+background-position: center; background-size: cover ; overflow: hidden; width: 100vw;height: 100vh;">
 	<div class="score_bloc">
 		{#if connected == true}
 			<div class="scores">
-				{player.pseudo} : {player.score} - {player2.pseudo} : {player2.score}
+				<span style="color:rgb(93, 215, 255);">{player.pseudo}</span>: <span id="player1_score">{player.score}</span> <span style="color:rgb(93, 215, 255);">|</span> <span style="color:rgb(93, 215, 255);">{player2.pseudo}</span>: <span id="player2_score">{player2.score}</span>
 			</div>
 		{/if} 
+	</div>
+	<div class="canvas-container">		
+		<canvas id="canvas" width={setting_game.canvas_width} height={setting_game.canvas_height}>
+		</canvas>
 		{#if connected == false}
-		<button class="play_button" on:click={connect}>PLAY</button>
+			{#if waiting_game == false}
+				<button class="play_button" on:click={connect}>PLAY</button>
+			{:else}
+				<button class="waiting_button" on:click={connect}>WAITING...</button>
+			{/if}
 		{/if}
 	</div>
-	<canvas id="canvas" width={setting_game.canvas_width} height={setting_game.canvas_height}>
-	</canvas>
 </body>
 <!-- {/if} -->
 
 <script>
 // @ts-nocheck
-
 	import { Ball } from './ball';
 	import { Player } from './player';
 	import { onMount } from 'svelte';
@@ -51,9 +54,12 @@ background-position: center; background-size: cover ; overflow: hidden; width: 1
 	let loading = false;
 	let connected = false;
 	let touched = 0;
+	let touched_player_1 = false;
+	let touched_player_2 = false;
+	let waiting_game = false;
   // debug
-  let room_id;
-  let started = 0;
+	let room_id;
+	let started = 0;
 
 
   async function test() //debug
@@ -63,27 +69,24 @@ background-position: center; background-size: cover ; overflow: hidden; width: 1
 
 function countdown(counter)
 {
-  const context = canvas.getContext('2d');
-  context.font = '80px Bebas Neue';
-  context.fillStyle = 'rgb(93, 215, 255)';
-  context.textAlign = 'center';
-  context.textBaseline = 'middle';
+	const context = canvas.getContext('2d');
+	context.font = '80px Bebas Neue';
+	context.fillStyle = 'rgb(93, 215, 255)';
+	context.textAlign = 'center';
+	context.textBaseline = 'middle';
 
-  const countdownInterval = setInterval(() => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillText(counter, canvas.width / 2, canvas.height / 2);
-
-    counter--;
-
-    if (counter < 0) {
-      clearInterval(countdownInterval);
-      play()
-    }
-  }, 1000);
+	const countdownInterval = setInterval(() => {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillText(counter, canvas.width / 2, canvas.height / 2);
+		counter--;
+		if (counter < 0) {
+		clearInterval(countdownInterval);
+		play()
+		}
+	}, 500);
 }
 
-function getRoomIdFromUrl()
-{
+function getRoomIdFromUrl(){
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get('room_id');
 }
@@ -98,8 +101,7 @@ function init_player2() {
   return player2;
 }
 
-function init_ball()
-{
+function init_ball(){
   let ball = new Ball(setting_game.canvas_width / 2, setting_game.canvas_height / 2, 2, 2);
   return ball;
 }
@@ -109,57 +111,60 @@ let player2 = init_player2();
 let ball = init_ball();
 
 // Fonction pour afficher le message de victoire
-function print_win(gagnant)
-{
-  // Effacer le canvas
-  const context = canvas.getContext('2d');
-  cancelAnimationFrame(anim);
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  
-  // Style du texte
-  context.font = '60px Bebas Neue';
-  context.fillStyle = 'rgb(93, 215, 255)';
-  context.textAlign = 'center';
-  context.textBaseline = 'middle';
-  
-  // Coordonnées du message
-  const x = canvas.width / 2;
-  const y = canvas.height / 2;
-  
-  // Afficher le message de victoire
-  context.fillText(`${gagnant} won! Score: ${player.score} | ${player2.score}`, x, y);
+function print_win(gagnant){
+	// Effacer le canvas
+	const context = canvas.getContext('2d');
+	cancelAnimationFrame(anim);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Style du texte
+	context.font = '60px Bebas Neue';
+	context.fillStyle = 'rgb(93, 215, 255)';
+	context.textAlign = 'center';
+	context.shadowColor="black";
+	context.shadowOffsetX = 5;
+	context.shadowOffsetY = 5;
+	context.shadowBlur = 6;
+	context.textBaseline = 'middle';
+
+	// Coordonnées du message
+	const x = canvas.width / 2;
+	const y = canvas.height / 2;
+
+	// Afficher le message de victoire
+	context.fillText(`${gagnant} won!`, x, y);
 }
-    
-async function connect()
-{
+
+async function connect(){
     Colyseus = await import("colyseus.js");
     client = new Colyseus.Client('ws://localhost:3001');
     room = await client.joinById(room_id);
+	waiting_game = true;
     room.onMessage('Player_init', (message) =>
     {
-      player.pseudo = message.player1_pseudo;
-      player2.pseudo = message.player2_pseudo;
-      player.id = message.player1_id;
-      player2.id = message.player2_id;
-      player.score = message.player1_score;
-      player2.score = message.player2_score;
-      player.color = message.player1_color;
-      player2.color = message.player2_color;
-      player.id_user = message.player1_iD_user;
-      player2.id_user = message.player2_iD_user;
-      player.username = message.player1_username;
-      player2.username = message.player2_username;
-      mainclient = message.mainclient;
-      console.log(mainclient);
-      showButtons = true;
-	  connected = true;
-      countdown(3);
+		player.pseudo = message.player1_pseudo;
+		player2.pseudo = message.player2_pseudo;
+		player.id = message.player1_id;
+		player2.id = message.player2_id;
+		player.score = message.player1_score;
+		player2.score = message.player2_score;
+		player.color = message.player1_color;
+		player2.color = message.player2_color;
+		player.id_user = message.player1_iD_user;
+		player2.id_user = message.player2_iD_user;
+		player.username = message.player1_username;
+		player2.username = message.player2_username;
+		mainclient = message.mainclient;
+		console.log(mainclient);
+		showButtons = true;
+		connected = true;
+		countdown(3);
     })
-  room.onMessage('role', (message) => {
-    clientId = message.client;
-  });
-  room.send('player_name', {player_pseudo : currentUser.pseudo, player_username : currentUser.username, player_id : currentUser.id});
-  console.log('Joined succefuly', room);
+	room.onMessage('role', (message) => {
+		clientId = message.client;
+	});
+	room.send('player_name', {player_pseudo : currentUser.pseudo, player_username : currentUser.username, player_id : currentUser.id});
+	console.log('Joined succefuly', room);
 }
     
 const MAX_SPEED = 8;
@@ -180,9 +185,7 @@ const fletchCurrentUserData = async () => {
 		}
 	};
 
-
-function draw()
-{
+function draw(){
 	const context = canvas.getContext('2d');
 	// Set the background color
 	context.fillStyle = 'rgba(35, 35, 35)';
@@ -193,6 +196,13 @@ function draw()
 	context.beginPath();
 	context.arc(ball.x, ball.y, setting_game.ball_radius, 0, Math.PI * 2, false);
 	context.fill();
+
+	//middle bar
+	context.strokeStyle = 'rgb(93, 215, 255)';
+	context.beginPath();
+	context.moveTo(canvas.width / 2, 0);
+	context.lineTo(canvas.width / 2, canvas.height);
+	context.stroke();
 
 	// Set the color of the bars
 	context.fillStyle = 'rgb(93, 215, 255)';
@@ -205,8 +215,7 @@ function draw()
 	);
 }
 
-function draw2()
-{
+function draw2(){
 	const context = canvas.getContext('2d');
 	// Set the background color
 	context.fillStyle = 'rgba(35, 35, 35)';
@@ -218,20 +227,40 @@ function draw2()
 	context.arc(ball.x, ball.y, setting_game.ball_radius, 0, Math.PI * 2, false);
 	context.fill();
 
+	//middle bar
+	context.strokeStyle = 'rgb(93, 215, 255)';
+	context.beginPath();
+	context.moveTo(canvas.width / 2, 0);
+	context.lineTo(canvas.width / 2, canvas.height);
+	context.stroke();
+
 	// Set the color of the bars
-	context.fillStyle = 'white';
-	context.fillRect(0, player.y, setting_game.paddle_width, setting_game.paddle_height);
-	context.fillRect(
-		canvas.width - setting_game.paddle_width,
-		player2.y,
-		setting_game.paddle_width,
-		setting_game.paddle_height
-	);
+	if (touched_player_1 == true){
+		context.fillStyle = 'white';
+		context.fillRect(0, player.y, setting_game.paddle_width, setting_game.paddle_height);
+		context.fillStyle = 'rgb(93, 215, 255)';
+		context.fillRect(
+			canvas.width - setting_game.paddle_width,
+			player2.y,
+			setting_game.paddle_width,
+			setting_game.paddle_height
+			);
+	}
+	else{
+		context.fillStyle = 'rgb(93, 215, 255)';
+		context.fillRect(0, player.y, setting_game.paddle_width, setting_game.paddle_height);
+		context.fillStyle = 'white';
+		context.fillRect(
+			canvas.width - setting_game.paddle_width,
+			player2.y,
+			setting_game.paddle_width,
+			setting_game.paddle_height
+			);
+	}
 	touched--;
 }
 
-function play()
-{
+function play(){
 	if (mainclient == true)
 		ballMove();
 	updatePos();
@@ -248,85 +277,93 @@ function play()
 
 function collide(playerCurrent) {
   // Le joueur ne touche pas la balle
-  if (ball.y < playerCurrent.y || ball.y > playerCurrent.y + setting_game.paddle_height){
-    if (ball.x > canvas.width - setting_game.paddle_width)
-      player.score++;
-    else
-      player2.score++;
-    // Remet la balle et les joueurs au centre
-    ball.x = canvas.width / 2;
-    ball.y = canvas.height / 2;
-    // envoie les scores.
-    room.send("updateScore", {player_score: player.score , player2_score : player2.score});
-    ball.velocity_x = (Math.random() * 3 + 2) * (Math.random() < .5 ? -1 : 1);
-		ball.velocity_y = Math.random();
-    // if(Math.abs(ball.velocity_x) < MAX_SPEED)
-    //   ball.velocity_x *= 1.2;
-  }
-  else{
-    // Augmente la vitesse et change la direction
-    if(Math.abs(ball.velocity_x) < MAX_SPEED)
+	if (ball.y < playerCurrent.y || ball.y > playerCurrent.y + setting_game.paddle_height){
+		if (ball.x > canvas.width - setting_game.paddle_width) {
+			const player1ScoreElement = document.getElementById("player1_score");
+			player1ScoreElement.classList.add('pop_out');
+			player.score++;
+			player1ScoreElement.addEventListener('animationend', function () {
+				player1ScoreElement.classList.remove('pop_out');
+				player1ScoreElement.classList.add('pop_in');
+			}, { once: true });
+		} else {
+			const player2ScoreElement = document.getElementById("player2_score");
+			player2ScoreElement.classList.add('pop_out');
+			player2.score++;
+			player2ScoreElement.addEventListener('animationend', function () {
+				player2ScoreElement.classList.remove('pop_out');
+			player2ScoreElement.classList.add('pop_in');
+			}, { once: true });
+		}
+		// Remet la balle et les joueurs au centre
+		ball.x = canvas.width / 2;
+		ball.y = canvas.height / 2;
+		// envoie les scores.
+		room.send("updateScore", {player_score: player.score , player2_score : player2.score});
+		ball.velocity_x = (Math.random() * 3 + 2) * (Math.random() < .5 ? -1 : 1);
+			ball.velocity_y = Math.random();
+		// if(Math.abs(ball.velocity_x) < MAX_SPEED)
+		//   ball.velocity_x *= 1.2;
+	}
+	else{
+	// Augmente la vitesse et change la direction
+	if(Math.abs(ball.velocity_x) < MAX_SPEED)
 		ball.velocity_x *= -1.1;
-    changeDirection(playerCurrent.y);
-	touched = 15;
-}
+	changeDirection(playerCurrent.y);
+	touched = 10;
+	}
 }
 
-function changeDirection(playerPosition)
-{
+function changeDirection(playerPosition){
   var impact = ball.y - playerPosition - setting_game.paddle_height / 2;
   var ratio = 100 / (setting_game.paddle_height / 2);
   // Obtenir une valeur entre 0 et 10
   ball.velocity_y = Math.round(impact * ratio / 10);
 }
 
-function endGame()
-{
+function endGame(){
   room.onMessage("gameFinished", (message) => {
     winner = message.winner;
     gameFinished = true;
     if (winner == player.pseudo && mainclient == true)
-    {
       print_win(winner);
-    }
     else
       print_win(winner);
   });
 }
 
-function Updatescore()
-{
+function Updatescore(){
   room.onMessage("updateScore", (message) => {
       player.score = message.player_score;
       player2.score = message.player2_score;
   })
 }
 
-function Updateball()
-{
+function Updateball(){
   room.onMessage("ballPos", (message) => {
     ball.x = message.ball_x;
     ball.y = message.ball_y;
   })
 }
 
-function ballMove()
-{
-  if (ball?.y > canvas.height || ball?.y < 0) {
+function ballMove(){
+  if (ball?.y > canvas.height || ball?.y < 0)
       ball.velocity_y *= -1;
-  }
   if (ball.x > canvas.width - setting_game.paddle_width) {
+	touched_player_2 = true;
+	touched_player_1 = false;
     collide(player2);
-  } else if (ball.x < setting_game.paddle_width) {
-    collide(player);
+} else if (ball.x < setting_game.paddle_width) {
+	collide(player);
+	touched_player_2 = false;
+	touched_player_1 = true;
   }
   ball.x += ball.velocity_x;
   ball.y += ball.velocity_y;
   room.send("ballPos", {ball_x: ball.x, ball_y: ball.y});
 }
 
-function stop()
-{
+function stop(){
   cancelAnimationFrame(anim);
   // Placez la balle et les joueurs au centre
   ball.x = canvas.width / 2;
@@ -344,8 +381,7 @@ function stop()
   draw();
 }
 
-function playerMove(event)
-{
+function playerMove(event){
   if ((clientId === player?.id))
   {
     var canvasLocation = canvas.getBoundingClientRect();
@@ -355,11 +391,10 @@ function playerMove(event)
       player.y = (mouseLocation / canvasLocation.height * canvas.height)
               - setting_game.paddle_height / 2;
     }
-
     // permet de limiter les players par rapport a la taille du canvas
     if (mouseLocation < setting_game.paddle_height / 2) {
-      if(player?.y || player?.y === 0)
-        player.y = 0;
+		if(player?.y || player?.y === 0)
+			player.y = 0;
     } else if ((mouseLocation / canvasLocation.height * canvas.height)
           + setting_game.paddle_height / 2 > canvas.height) {
       if(player?.y || player?.y === 0)
@@ -372,8 +407,7 @@ function playerMove(event)
   }
 }
 
-function player2Move(event)
-{
+function player2Move(event){
   if (clientId === player2?.id) {
     var canvasLocation = canvas.getBoundingClientRect();
     var mouseLocation = event.clientY - canvasLocation.y;
@@ -382,7 +416,6 @@ function player2Move(event)
       player2.y = (mouseLocation / canvasLocation.height * canvas.height)
               - setting_game.paddle_height / 2;
     }
-
     // permet de limiter les players par rapport a la taille du canvas
     if (mouseLocation < setting_game.paddle_height / 2) {
       if(player2?.y || player2?.y === 0)
@@ -398,15 +431,13 @@ function player2Move(event)
     }
   }
 }
-// Obtenir l'emplacement de la souris dans le canevas
-function updatePos()
-{
+// Obtenir l'emplacement de la souris dans le canvas
+function updatePos(){
   if (room)
   {
     room.onMessage("player", (message) => {
       player.y = message.player_y;
     })
-
     room.onMessage("player2", (message) => {
       player2.y = message.player2_y;
     })
