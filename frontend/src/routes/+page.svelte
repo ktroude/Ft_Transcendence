b4bd959d16d632a5b65278b6facf8b904ff035ea
Paco Le berre <pacocmoi@live.fr>
2023-05-29 16:29:00 +0200

    merge simpson

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,5 +3,5 @@
 <h2>
     clique <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-50f3e66e6fe031e7c81489010de827d9db8e756fa49de5d8760e1722d283c275&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ftoken&response_type=code">
         ici 
-    </a> pour te connecter 
+    </a> ou <a href="">la</a> pour te connecter breeeeeeeeeeeef
 </h2>
