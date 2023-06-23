e93c120f6a9cbe948bd2a53c40bd83f7fb6ac975
Benoit Leroy <bleroy@c2r8p1.42nice.fr>
2023-06-23 16:36:35 +0200

    MaxInt error handling

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -15,10 +15,8 @@
   import { page } from '$app/stores';
 </script>
 
-
-
 <h1 class="Ft-Transcendence"> {$page.status}</h1>
 <h2 class="Ft-Transcendence"> Page not found</h2>
-<h3 class="glow"> You looking for something that doesn't exist anymore...</h3>
+<h3 class="page404"> You looking for something that doesn't exist anymore...</h3>
 
 <button class="back_to_lobby" on:click={() => location.href = '/homepage'}>Back to Homepage<span></span></button>
